7c4e9ff51b162e556df4516051accdc1a8e3c15d
klepto <klepto@inbox.lt>
2025-11-21 21:08:06 +0200

    chore: Change default router settings

--- src/index.js
+++ src/index.js
@@ -1,14 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import {BrowserRouter, Route, Routes} from "react-router";
+import {BrowserRouter} from "react-router";
 
 const root = document.getElementById("root");
 
 ReactDOM.createRoot(root).render(
 	<BrowserRouter>
-		<Routes>
-			<Route path="/" element={<App />} />
-		</Routes>
+        <App />
 	</BrowserRouter>,
 );
